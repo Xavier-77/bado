453a5219dc4c76eeb861048a8a135b40f2f847d2
BADO Xavier <badoxavierzwamassoe@gmail.com>
2023-08-07 10:56:09 +0000

    ajout de traitement

---
+++ traitement.php
@@ -0,0 +1,24 @@
+<?php
+function removeSpecialChars($text) {
+    // Supprimer les accents
+    $text = iconv('UTF-8', 'ASCII//TRANSLIT', $text);
+    
+    // Supprimer les apostrophes
+    $text = str_replace("'", "", $text);
+    
+    // Supprimer les caractères spéciaux
+    $text = preg_replace('/[^a-zA-Z0-9\s]/', '', $text);
+    
+    return $text;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["texte"])) {
+        $texteOriginal = $_POST["texte"];
+        $texteTraite = removeSpecialChars($texteOriginal);
+
+        print("Texte original : " . $texteOriginal ."\n");
+        print("Texte traité : " . $texteTraite );
+    }
+}
+?>
